222279e9ae118a89bbde8b344a9d763642c6f92a
Emiliano <emrdz56@gmail.com>
2024-11-20 15:11:13 -0600

    correcciones practica 1

--- Practica 1/main.py
+++ Practica 1/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from tabulate import tabulate
 
-df = pd.read_csv('C:/Users/emili/OneDrive/Documentos/Mineria-de-datos/df_whastapp.csv')
+df = pd.read_csv('C:/Users/emili/OneDrive/Documentos/Mineria-de-datos/chats_whastapp.csv')
 print(tabulate(df, headers='keys', tablefmt='pretty'))
