7d850adea9b501af1ba1e295b01ede6ca860251d
Emiliano <emrdz56@gmail.com>
2024-11-22 00:21:59 -0600

    cambios practica 2

--- Practica 2/Practica2.py
+++ Practica 2/Practica2.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
-# Leer el archivo CSV asegurando la correcta codificación para acentos y 'Ñ'
-df = pd.read_csv(r'C:\Users\emili\OneDrive\Documentos\Mineria-de-datos\df_whastapp.csv', encoding='utf-8')
+# Cargar el CSV
+ruta_csv = 'df_whastapp.csv'
+df = pd.read_csv(ruta_csv)
 
-# Separar por columnas (suponiendo que el archivo ya esté organizado de forma limpia)
-df.columns = ['fecha', 'hora', 'autor', 'mensaje']
+# Convertir las columnas 'fecha' y 'hora' a un solo campo de tipo datetime
+df['datetime'] = pd.to_datetime(df['fecha'] + ' ' + df['hora'], dayfirst=True)
 
-# Mostrar las primeras filas para verificar
-print(df.head())
+# Ordenar el dataframe por la nueva columna 'datetime'
+df_sorted = df.sort_values(by='datetime')
+
+# Guardar el archivo CSV ordenado
+df_sorted.to_csv('df_whastapp_2.csv', index=False)
+
+print("Archivo ordenado correctamente.")
